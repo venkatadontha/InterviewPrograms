10993307a1d28f257e617d52a327924d54038d50
Vivaan-PC\Vivaan <j.kousalya@gmail.com>
2019-07-11 00:01:43 +0100

    Strings Class added

---
+++ InterviewScenarios/src/InterviewScenarios/Strings.java
@@ -0,0 +1,11 @@
+package InterviewScenarios;
+
+public class Strings {
+	
+	public static void main(Strings[] args)
+	{
+		String str = "My Name is Venkata Raja Kishore Dontha";
+		System.out.println(str.length());
+	}
+
+}
